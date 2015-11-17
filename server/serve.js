41ec9dd0a9ec357fe2d1208aa9f952e49ff8753c
Hutson Betts <hutson@hyper-expanse.net>
2015-11-16 23:14:02 -0600

    feat(serve): Add support for PORT environmental variable.

--- server/serve.js
+++ server/serve.js
@@ -7,10 +7,17 @@ var app = express()
 	.use(require('./index'));
 
 var port = process.argv.slice(2)[0];
-if (!port) {
-  port = 9913;
+if (port) {
+  startServer(port);
+} else if (process.env.PORT) {
+  startServer(process.env.PORT);
+} else {
+  winston.error('Invalid port value provided. Either a port must be provided as an argument, ' +
+      'or as the PORT environmental variable.');
 }
 
-var server = app.listen(port, function() {
-  winston.log('Listening on port %d...', server.address().port);
-});
+function startServer(port) {
+  var server = app.listen(port, function() {
+    winston.info('Listening on port %d...', server.address().port);
+  });
+}
