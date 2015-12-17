4b43c60484b445504d1b352169ea4a365bbee9c4
Hutson Betts <hutson@hyper-expanse.net>
2015-12-17 16:30:12 -0600

    test(jenkins): Fix tests to match expectations.

--- server/index.spec.js
+++ server/index.spec.js
@@ -7,43 +7,57 @@ var request = require('supertest');
 
 var app = require('./index');
 
-function generateJenkinsReponse(protocol) {
-  return nock(protocol + '://d7.mnpk.org')
+function generateJenkinsReponse() {
+  return nock('https://d7.mnpk.org')
     .get('/jenkins/job/goyo/lastSuccessfulBuild/cobertura/api/json/')
     .query({ depth: 2 });
 }
 
-describe('coverage results', function() {
+describe('jenkins coverage badge service', function() {
+  var env;
+
+  beforeEach(function() {
+    env = process.env;
+
+    process.env.JENKINS_SERVER = 'https://d7.mnpk.org/jenkins/';
+  });
 
   afterEach(function() {
-    nock.cleanAll();
+    process.env = env;
   });
 
-  it('return coverage none when jenkins job doesn\'t exist', function(done) {
-    var jenkins = generateJenkinsReponse('http')
-      .reply(404);
+  describe('coverage results', function() {
 
-    request(app)
-      .get('/jenkins/cobertura/http/d7.mnpk.org/jenkins/job/goyo')
-      .expect(302)
-      .expect('Location', 'https://img.shields.io/badge/coverage-none-lightgrey.svg')
-      .end(function(err) {
-        jenkins.done();
-        done(err);
-      });
-  });
+    afterEach(function() {
+      nock.cleanAll();
+    });
 
-  it('returns a 302 redirect on 100 test coverage', function(done) {
-    var jenkins = generateJenkinsReponse('http')
-      .reply(200, require('./cobertura.100.json'));
+    it('return coverage none when jenkins job doesn\'t exist', function(done) {
+      var jenkins = generateJenkinsReponse('http')
+        .reply(404);
 
-    request(app)
-      .get('/jenkins/cobertura/http/d7.mnpk.org/jenkins/job/goyo')
-      .expect(302)
-      .expect('Location', 'https://img.shields.io/badge/coverage-100%-brightgreen.svg')
-      .end(function(err) {
-        jenkins.done();
-        done(err);
-      });
+      request(app)
+        .get('/jenkins/cobertura/job/goyo')
+        .expect(302)
+        .expect('Location', 'https://img.shields.io/badge/coverage-none-lightgrey.svg')
+        .end(function(err) {
+          jenkins.done();
+          done(err);
+        });
+    });
+
+    it('returns a 302 redirect on 100 test coverage', function(done) {
+      var jenkins = generateJenkinsReponse('http')
+        .reply(200, require('./cobertura.100.json'));
+
+      request(app)
+        .get('/jenkins/cobertura/job/goyo')
+        .expect(302)
+        .expect('Location', 'https://img.shields.io/badge/coverage-100%-brightgreen.svg')
+        .end(function(err) {
+          jenkins.done();
+          done(err);
+        });
+    });
   });
 });
