ce9f3c605ddb7f4d1f42c5354233f69bc7e3515f
Hutson Betts <hutson@hyper-expanse.net>
2016-01-16 16:07:06 -0600

    style(serve): Correct spacing issues.

--- server/serve.js
+++ server/serve.js
@@ -4,7 +4,7 @@ var express = require('express');
 var winston = require('winston');
 
 var app = express()
-	.use(require('./index'));
+  .use(require('./index'));
 
 var port = process.argv.slice(2)[0];
 if (port) {
@@ -13,7 +13,7 @@ if (port) {
   startServer(process.env.PORT);
 } else {
   winston.error('Invalid port value provided. Either a port must be provided as an argument, ' +
-      'or as the PORT environmental variable.');
+    'or as the PORT environmental variable.');
 }
 
 function startServer(port) {
